650b034bf721323848b4b9952b1b6d6d0984553c
Surobhi <Surobhi@Surobhi>
2016-07-07 22:08:22 +0530

    output modified

--- simpleservlet/src/hcl/simpleservlet/SecondServlet.java
+++ simpleservlet/src/hcl/simpleservlet/SecondServlet.java
@@ -21,7 +21,7 @@ import javax.servlet.http.HttpServletResponse;
 			
 			String n=request.getParameter("userName");
 			
-			pw.println("welcome to this page"+n);
+			pw.println("welcome to this page now"+n);
 
 }
 	}
