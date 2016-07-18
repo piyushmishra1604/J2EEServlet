ba2f36f17ddfc979c6e1bf95d6c8a6684eb91f59
SOFIA <SOFIA@SOFIA-HP>
2016-07-18 19:03:17 +0530

    database Configured

---
+++ Login/src/com/hcl/login/TestJdbc.java
@@ -0,0 +1,28 @@
+package com.hcl.login;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class TestJdbc {
+
+	
+	public static void main(String[] args) throws Exception {
+		
+		
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		
+		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","hr","hr");
+		
+		Statement stmt=con.createStatement();
+		
+		ResultSet rs=stmt.executeQuery("select * from employees");
+		
+		while(rs.next())
+		System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
+		
+		
+		con.close();
+	}
+}
